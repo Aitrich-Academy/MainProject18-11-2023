3d2a1a252274b227fb4f6fe91faced35f6171722
thisisdevikasunilkumar <devikasunilkumar20@gmail.com>
2023-11-19 14:26:55 +0530

    MainProject

---
+++ MainProject/MainProject/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace MainProject.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
