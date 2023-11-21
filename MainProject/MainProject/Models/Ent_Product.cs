de6ea2feffb68f859b8a0383a7ecd7700535046f
thisisdevikasunilkumar <devikasunilkumar20@gmail.com>
2023-11-21 09:29:55 +0530

    Product Manager

---
+++ MainProject/MainProject/Models/Ent_Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MainProject.Models
+{
+    public class Ent_Product
+    {
+        public int ProductID { get; set; }
+        public string Product_Name { get; set; }
+        public decimal Price { get; set; }
+        public string Image { get; set; }
+        public int Category_id { get; set; }
+        public string Status { get; set; }
+    }
+}
