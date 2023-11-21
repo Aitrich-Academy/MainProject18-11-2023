49ea4b7cd1283f696429b678c25bda14d48b1282
thisisdevikasunilkumar <devikasunilkumar20@gmail.com>
2023-11-21 10:26:02 +0530

    User Manager

--- MainProject/DAL/Manager/UserManager.cs
+++ MainProject/DAL/Manager/UserManager.cs
@@ -5,11 +5,10 @@ using System.Linq;
 using System.Runtime.Remoting.Contexts;
 using System.Text;
 using System.Threading.Tasks;
-using DAL.Model;
+using DAL.Models;
 
 namespace DAL.Manager
-{
- 
+{ 
     public class UserManager
     {
         Model1 User_db = new Model1();
@@ -17,13 +16,12 @@ namespace DAL.Manager
         public string UserRegister(UsersRegister rej)
         {
             int result = 0;
-            if (rej != null) 
+            if (rej != null)
             {
-                UsersRegister objct=new UsersRegister();
+                UsersRegister objct = new UsersRegister();
 
                 objct.Name = rej.Name;
                 objct.Email = rej.Email;
-            
                 objct.PhoneNumber = rej.PhoneNumber;
                 objct.District = rej.District;
                 objct.Pincode = rej.Pincode;
@@ -34,16 +32,60 @@ namespace DAL.Manager
                 result = User_db.SaveChanges();
 
             }
-            if(result>0)
+            if (result > 0)
             {
                 return "Successfully Registered";
-
             }
             else
             {
                 return "Error";
             }
         }
+        public List<Product> View()
+        {
+            return User_db.Products.ToList();
+        }
 
+        public string Update(int id, UsersRegister register)
+        {
+            string result = "";
+            UsersRegister update = User_db.UsersRegisters.Where(e => e.UserID == id && e.Status != "Delete").FirstOrDefault();
+            if (update != null)
+            {
+                update.Name = register.Name;
+                update.Email = register.Email;
+                update.PhoneNumber = register.PhoneNumber;
+                update.District = register.District;
+                update.Pincode = register.Pincode;
+                update.PasswordHash = register.PasswordHash;
+                update.Status = "Active";
+
+                User_db.Entry(update).State = EntityState.Modified;
+                result = User_db.SaveChanges().ToString();
+                result = "Success";
+            }
+            else
+            {
+                result = "Error";
+            }
+            return result;
+        }
+
+        public string Delete(int id)
+        {
+            string result = "";
+            UsersRegister delete = User_db.UsersRegisters.FirstOrDefault(e => e.UserID == id && e.Status != "Delete");
+            if (delete != null)
+            {
+                delete.Status = "Delete";
+                User_db.SaveChanges();
+                result = "Success";
+            }
+            else
+            {
+                result = "Product not found";
+            }
+            return result;
+        }     
     }
 }
