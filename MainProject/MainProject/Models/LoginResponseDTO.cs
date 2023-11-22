b2315fbab155920e5290311d76160828b2fb6f62
ashilymariya <ashilymariyapv@gmail.com>
2023-11-22 14:32:50 +0530

    token add

--- MainProject/MainProject/Models/LoginResponseDTO.cs
+++ MainProject/MainProject/Models/LoginResponseDTO.cs
@@ -8,12 +8,13 @@ namespace MainProject.Models
     public class LoginResponseDTO
     {
         public string Token { get; set; }
-        public int user_id { get; set; }
+        public int? user_id { get; set; }
         public string name { get; set; }
         public string email { get; set; }
         public long phone { get; set; }
         public string district { get; set; }
-      
+        public byte[] image { get; set; }
         public string role { get; set; }
+    }
     }
 }
--- MainProject/MainProject/Utils/TokenManager.cs
+++ MainProject/MainProject/Utils/TokenManager.cs
@@ -1,8 +1,10 @@
 ﻿using DAL.Models;
 using System;
 using System.Collections.Generic;
+using Microsoft.IdentityModel.Tokens;
 using System.Linq;
 using System.Security.Claims;
+using System.IdentityModel.Tokens.Jwt;
 using System.Web;
 
 namespace MainProject.Utils
@@ -11,7 +13,7 @@ namespace MainProject.Utils
     {
         private static string Secret = "ERMN05OPLoDvbTTa/QkqLNMI3cPLguaRyHzyg7n5qNBVjQmtBhz4SzYh4NBVCXi3KJHlSXKP+oi2+bXr6CUYTR==";
 
-        public static string GenerateToken(UsersRegister UsersBO)
+        public static string GenerateToken(UsersRegister Users)
         {
             byte[] key = Convert.FromBase64String(Secret);
             SymmetricSecurityKey securityKey = new SymmetricSecurityKey(key);
@@ -21,8 +23,8 @@ namespace MainProject.Utils
                 Issuer = "MyProjectName",
                 Subject = new ClaimsIdentity(
                     new[] {
-                        new Claim("USER_ID", UsersBO.USER_ID.ToString()),
-                        new Claim("USER_ROLE", UsersBO.USER_ROLE.ToString())
+                        new Claim("UserID", Users.UserID.ToString()),
+                        new Claim("Roll", Users.Roll.ToString())
                     }),
                 Expires = DateTime.UtcNow.AddMinutes(TokenExpiry),
                 SigningCredentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256Signature)
@@ -31,4 +33,5 @@ namespace MainProject.Utils
             JwtSecurityToken token = handler.CreateJwtSecurityToken(descriptor);
             return handler.WriteToken(token);
         }
+    }
 }
