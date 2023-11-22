5e834faa12ebb9c52bad9e62e83ad1833ca8384c
thisisdevikasunilkumar <devikasunilkumar20@gmail.com>
2023-11-22 10:43:24 +0530

    Order

--- MainProject/MainProject/Controllers/OrderController.cs
+++ MainProject/MainProject/Controllers/OrderController.cs
@@ -14,17 +14,16 @@ namespace MainProject.Controllers
     [RoutePrefix("api/Order")]
     public class OrderController : ApiController
     {
+        #region Confirm Order
         [System.Web.Http.AcceptVerbs("GET", "POST")]
         [System.Web.Http.HttpGet]
         [Route("BookingDetail")]   // Url creation Route
         [HttpPost]
         public string BookingDetail(Ent_Order order)
         {
-            OrderManager manager = new OrderManager();
-            
+            OrderManager manager = new OrderManager();            
             Ent_Order order2 = order;
             Order order1 = new Order();
-            
             order1.OrderID = order2.OrderID;
             order1.User__id = order2.Userid;
             order1.Category_id = order2.category_id;
@@ -34,22 +33,19 @@ namespace MainProject.Controllers
             order1.Price = order2.Price;
             order1.Quantity = order2.Quantity;
             order1.Total_Price = order2.Quantity* productprice;
-
             order1.Image = order2.Image;
             order1.OrderDate = DateTime.Now;
             return manager.BookingDetail(order1);
-
         }
+        #endregion
 
-       
-
-        #region View all order
+        #region View all Order
         [System.Web.Http.AcceptVerbs("GET", "POST")]
         [System.Web.Http.HttpGet]
         [Route("View")]
         public List<Ent_Order> ViewUser()
         {
-           OrderManager manager= new OrderManager();
+            OrderManager manager= new OrderManager();
             List<Ent_Order> return_List = new List<Ent_Order>();
             List<Order> table_user = manager.View();
             if (table_user.Count != 0)
@@ -58,28 +54,25 @@ namespace MainProject.Controllers
                 {
                     return_List.Add(new Ent_Order
                     {
-                   OrderID=obj.OrderID,
-                    Userid=(int)obj.User__id,
-                    category_id=(int)obj.Category_id,
-                    ProductID=(int)obj.Product_id,
-                    ProductName=obj.Product_Name,
-                    Price=obj.Price,
-                    Quantity=obj.Quantity,
-                    Total_Price=obj.Total_Price,
-                    Image=obj.Image,
-                    OrderDate=DateTime.Now,
-                    Status=obj.Status
-                   
-
-                  });
+                        OrderID = obj.OrderID,
+                        Userid = (int)obj.User__id,
+                        category_id = (int)obj.Category_id,
+                        ProductID = (int)obj.Product_id,
+                        ProductName = obj.Product_Name,
+                        Price = obj.Price,
+                        Quantity = obj.Quantity,
+                        Total_Price = obj.Total_Price,
+                        Image = obj.Image,
+                        OrderDate = DateTime.Now,
+                        Status = obj.Status                 
+                    });
                 }
             }
             return return_List;
         }
         #endregion
 
-
-        #region User Delete
+        #region Cancel Order
         [System.Web.Http.AcceptVerbs("GET", "POST")]
         [System.Web.Http.HttpGet]
         [Route("DeleteOrder")]
@@ -98,6 +91,5 @@ namespace MainProject.Controllers
             }
         }
         #endregion
-
     }
 }
