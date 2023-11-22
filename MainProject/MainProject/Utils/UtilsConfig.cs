dbe80c9cc55a58db3e488b1fd597e50112a1a1e7
ashilymariya <ashilymariyapv@gmail.com>
2023-11-22 14:52:24 +0530

    utilsconfig

---
+++ MainProject/MainProject/Utils/UtilsConfig.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http.ModelBinding;
+
+namespace MainProject.Utils
+{
+    public class UtilsConfig
+    {
+        public static List<string> GetErrorListFromModelState(ModelStateDictionary modelState)
+        {
+            var query = from state in modelState.Values
+                        from error in state.Errors
+                        select error.ErrorMessage;
+
+            var errorList = query.ToList();
+            return errorList;
+        }
+    }
+}
