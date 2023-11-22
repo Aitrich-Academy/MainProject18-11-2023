47c07dd51423088ce80a514b8d68b7d91b36151b
ashilymariya <ashilymariyapv@gmail.com>
2023-11-22 13:28:56 +0530

    User Manager

--- MainProject/MainProject/Models/Ent_UserRegistration.cs
+++ MainProject/MainProject/Models/Ent_UserRegistration.cs
@@ -9,11 +9,13 @@ namespace MainProject.Models
     {
         public int id { get; set; }
         public string name { get; set; }
-        public string email { get; set; } 
+        public string email { get; set; }
+        public long phonenumber { get; set; }
         public string district {  get; set; }
-        public long phonenumber {get; set; }
+        public long pincode { get; set; }
+        public string roll { get; set; }
+        public string profile_image { get; set; }
         public string passwordHash {  get; set; }
-        public long pincode {  get; set; }
         public string status { get; set; }
     }
 }
